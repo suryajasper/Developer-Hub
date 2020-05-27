62cc0f9f95bdd8c8ac57d79ad3f62e49a1a45f3d
Surya Jasper <suryajasper2004@gmail.com>
2020-05-26 22:59:36 -0700

    added ability to create lists in editor
    also added a BUNCH of bug fixes

--- client/resources/js/main.js
+++ client/resources/js/main.js
@@ -93,6 +93,7 @@ function addNewSection() {
   var sectDiv = document.createElement('div');
 
   var fieldset = document.createElement('fieldset');
+  fieldset.style.marginBottom = '350px';
   var legend = document.createElement('legend');
   var legendHeader = document.createElement('h1');
   legendHeader.style.display = "inline-block";
@@ -104,7 +105,7 @@ function addNewSection() {
   legend.appendChild(legendHeader);
   legend.appendChild(legendInput);
   fieldset.appendChild(legend);
-  var newDropdown = dropdown('Add new element', ['<h2>heading 1</h2>', '<p class = "big">heading 2</p>', '<p class = "medium">large text</p>', '<p>normal text</p>', '<p style = "color: blue;"><u>link<u></p>', '<code>code</code>'], ['Heading 1', 'Heading 2', 'Large text', 'Normal text', null, null]);
+  var newDropdown = dropdown('Add new element', ['<h2>heading 1</h2>', '<p class = "big">heading 2</p>', '<p class = "medium">large text</p>', '<p>normal text</p>', '<p style = "color: blue;"><u>link<u></p>', '<code>code</code>', 'unordered list', 'ordered list', 'end list'], ['Heading 1', 'Heading 2', 'Large text', 'Normal text', null, null, null, null, null]);
   var dropdiv = newDropdown.dd;
   var anchors = newDropdown.aa;
   console.log(anchors);
@@ -118,6 +119,8 @@ function addNewSection() {
       var anchparams = {};
       if (this.innerHTML === '<code>code</code>') {
         anchparams['code'] = document.createElement('textarea');
+      } else if (this.innerHTML === '<p>normal text</p>') {
+        anchparams['content'] = document.createElement('textarea');
       } else if (this.innerHTML === '<p style="color: blue;"><u>link<u></u></u></p>') {
         anchparams['name'] = document.createElement('input');
         anchparams['name'].type = 'text';
@@ -125,14 +128,27 @@ function addNewSection() {
         anchparams['link'] = document.createElement('input');
         anchparams['link'].style.display = 'inline-block';
         anchparams['link'].type = 'text';
-      }
-      else {
-        anchparams[this.id] = document.createElement('input');
-        anchparams[this.id].type = "text";
+      } else if (this.innerHTML === 'unordered list' || this.innerHTML === 'ordered list') {
+        anchparams['legend'] = document.createElement('h2');
+        anchparams['legend'].classList.add('listLegend');
+        anchparams['legend'].innerHTML = '<span>Start of new ' + this.innerHTML + '</span>';
+      } else if (this.innerHTML === 'end list') {
+        anchparams['legend'] = document.createElement('h2');
+        anchparams['legend'].classList.add('listLegend');
+        anchparams['legend'].innerHTML = '<span>End of list</span>';
+      } else {
+        anchparams['content'] = document.createElement('input');
+        anchparams['content'].type = "text";
       }
       var type = this.innerHTML;
       for (var key of Object.keys(anchparams)) {
-        anchparams[key].placeholder = key;
+        if (this.innerHTML !== 'unordered list' && this.innerHTML !== 'ordered list') {
+          if (this.id !== "" && this.id !== null) {
+            anchparams[key].placeholder = this.id;
+          } else {
+            anchparams[key].placeholder = 'content';
+          }
+        }
         fieldset.insertBefore(anchparams[key], dropdiv);
       }
       elementTypes.push(this.innerHTML);
@@ -152,9 +168,13 @@ function addNewSection() {
 
     var stringToAppend = '';
 
+    var startedParsingOrderedList = false;
+    var startedParsingUnorderedList = false;
+
     for (var i = 0; i < elementTypes.length; i++) {
       var elementString = null;
       console.log(elementTypes[i]);
+      console.log(elementParams[i]);
       switch (elementTypes[i]) {
         case '<code>code</code>':
           elementString = '<pre class="language-javascript" data-src-loaded="" data-src="../resources/prism/prism.js"><code class="language-javascript">' + elementParams[i]['code'].value + '</code></pre>';
@@ -174,8 +194,27 @@ function addNewSection() {
         case '<p style="color: blue;"><u>link<u></u></u></p>':
           elementString = '<a href = "' + elementParams[i]['link'].value + '">' + elementParams[i]['name'].value + '</a>';
           break;
+        case 'unordered list':
+          elementString = '<ul>';
+          startedParsingUnorderedList = true;
+          break;
+        case 'ordered list':
+          elementString = '<ol>';
+          startedParsingOrderedList = true;
+          break;
+        case 'end list':
+          if (startedParsingOrderedList)
+            elementString = '</ol>';
+          else if (startedParsingUnorderedList)
+            elementString = '</ul>';
+          startedParsingOrderedList = false;
+          startedParsingUnorderedList = false;
+          break;
       }
       console.log(elementString);
+      if ((startedParsingOrderedList && elementString !== '<ol>') || (startedParsingUnorderedList && elementString !== '<ul>')) {
+        elementString = '<li>' + elementString + '</li>';
+      }
       stringToAppend += elementString;
     }
 
